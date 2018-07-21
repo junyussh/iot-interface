b5e4a438ab77109d364e3591645f55a93f2c0249
Chun Yu <junyussh@gmail.com>
2018-07-21 13:20:35 +0800

    Dashboard 切版 登入後自動跳轉

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ const CleanSession = () => {
 function isLogin() {
   return new Promise((resolve) => {
     if (localStorage.getItem("token")) {
-      get(URL + "/user?token=" + localStorage.getItem("token")).then((res) => {
+      get("/user?token=" + localStorage.getItem("token")).then((res) => {
         if (res._id) {
-          console.log("true");
           resolve(true);
         } else {
           CleanSession();
@@ -23,6 +22,12 @@ function isLogin() {
       resolve(false);
     }
   })
+}
+function loadData(e) {
+  console.log(e)
+  get("/device?token=" + localStorage.getItem("token")).then((res) => {
+    console.log(res)
+  });
 }
 function getValue() {
   var form = document.querySelector("form");
@@ -38,16 +43,15 @@ function getValue() {
 
 function login(e) {
   let data = getValue();
-  post(data, "http://35.236.160.136/api/login").then((res) => {
+  post(data, "/login").then((res) => {
     let message = document.querySelector("#message");
     if (res.error) {
       message.innerHTML = ErrorMessageBox("錯誤的使用者名稱或密碼！");
     } else {
       e.classList.add("loading", "disabled");
       message.innerHTML = PostiveMessageBox("登入成功", "正在為您跳轉……");
-      console.log(res.token);
       SaveSession(res.token);
-      isLogin();
+      window.location = "./dashboard.html";
     }
   });
 }
@@ -72,7 +76,7 @@ const PostiveMessageBox = (title, message) => {
 
 function register() {
   let data = getValue();
-  post(data, "http://35.236.160.136/api/user").then((res) => {
+  post(data, "/user").then((res) => {
     let message = document.querySelector("#message");
     if (res.error) {
       message.innerHTML = ErrorMessageBox("使用者名稱已存在");
@@ -83,7 +87,7 @@ function register() {
 }
 
 function post(data, url) {
-  return fetch(url, {
+  return fetch(URL + url, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
@@ -95,7 +99,7 @@ function post(data, url) {
     .then(response => response.json())
 }
 function get(url) {
-  return fetch(url).then(response => response.json());
+  return fetch(URL + url).then(response => response.json());
 }
 document.querySelector("form").addEventListener("submit", function (event) {
   event.preventDefault();
